b9caf7d10f446e0eebb8ad387e0717b56922ee22
Agis Maulana <agismaulana0603@gmail.com>
2021-03-10 05:05:24 +0700

    commit node/vendor

---
+++ storage/framework/views/89f29b6e8682de57180d4b492106559c4bfeef67.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Deposits</title>
+	<link rel="icon" href="<?php echo e(asset('images/logo.ico')); ?>" type="image/x-icon" />
+	<link rel="stylesheet" type="text/css" href="<?php echo e(asset('css/app.css')); ?>">
+</head>
+<body>
+	<div id="app"></div>
+	<script type="text/javascript" src="<?php echo e(asset('js/app.js')); ?>"></script>
+</body>
+</html><?php /**PATH C:\Users\Agis\Documents\Ujikom\tabungan\resources\views/app.blade.php ENDPATH**/ ?>
